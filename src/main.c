2029a614f434dee9b9573bc77858471ea5a955c7
deep <166912166+deep9977@users.noreply.github.com>
2024-08-08 09:54:09 -0400

    Expanded instruction set with nop, dup, cmp, and variations of jump instruction

--- src/main.c
+++ src/main.c
@@ -8,14 +8,23 @@
 
 
 typedef enum{
+    CO_EQUAL,
+    CO_GREATER,
+    CO_LESS
+}Cmp_Outcome;
+
+typedef enum{
     TRAP_OK,
     TRAP_STACK_OVERFLOW,
     TRAP_STACK_UNDERFLOW,
     TRAP_INVALID_JMP,
     TRAP_DIVI_BY_ZERO,
+    TRAP_INDEX_OUT_OF_BOUND,
+    TRAP_UNEXPECTED_COMPARISON_OUTCOME
 }Trap;
 
 typedef enum {
+    INST_NOP,
     INST_PUSH,
     INST_POP,
     INST_ADD,
@@ -23,6 +32,15 @@ typedef enum {
     INST_MUL,
     INST_DIVI,
     INST_JMP,
+    INST_DUP,
+    INST_CMP,
+    INST_JE, INST_JZ,
+    INST_JNE, INST_JNZ,
+    INST_JG,
+    INST_JL,
+    INST_JGE, INST_JNL,
+    INST_JLE, INST_JNG,
+
 
 }Inst_Type;
 
@@ -40,6 +58,7 @@ typedef struct{
 }VM;
 
 
+#define inst_nop() ((Inst){.type = INST_NOP}) 
 #define inst_push(operand1) ((Inst){.type = INST_PUSH, .operand = operand1}) 
 #define inst_pop() ((Inst){.type = INST_POP}) 
 #define inst_add() ((Inst){.type = INST_ADD})
@@ -47,6 +66,18 @@ typedef struct{
 #define inst_mul() ((Inst){.type = INST_MUL})
 #define inst_divi() ((Inst){.type = INST_DIVI})
 #define inst_jmp(operand1) ((Inst){.type = INST_JMP, .operand = operand1})
+#define inst_dup(operand1) ((Inst){.type = INST_DUP, .operand = operand1})
+#define inst_cmp() ((Inst){.type = INST_CMP})
+#define inst_jz(operand1) ((Inst){.type = INST_JZ, .operand = operand1})
+#define inst_je(operand1) ((Inst){.type = INST_JE, .operand = operand1})
+#define inst_jnz(operand1) ((Inst){.type = INST_JNZ, .operand = operand1})
+#define inst_jne(operand1) ((Inst){.type = INST_JNE, .operand = operand1})
+#define inst_jg(operand1) ((Inst){.type = INST_JG, .operand = operand1})
+#define inst_jl(operand1) ((Inst){.type = INST_JL, .operand = operand1})
+#define inst_jge(operand1) ((Inst){.type = INST_JGE, .operand = operand1})
+#define inst_jnl(operand1) ((Inst){.type = INST_JNL, .operand = operand1})
+#define inst_jng(operand1) ((Inst){.type = INST_JGE, .operand = operand1})
+#define inst_jle(operand1) ((Inst){.type = INST_JNL, .operand = operand1})
 Inst Program[]= {
 
 };
@@ -78,6 +109,7 @@ void load_line(Inst inst, VM *vm){
 }
 
 Inst_Type cstr_to_inst(char *str){
+    if(strcmp(str, "nop") == 0) return INST_NOP; 
     if(strcmp(str, "push") == 0) return INST_PUSH; 
     if(strcmp(str, "pop") == 0) return INST_POP; 
     if(strcmp(str, "add") == 0) return INST_ADD;
@@ -85,7 +117,19 @@ Inst_Type cstr_to_inst(char *str){
     if(strcmp(str, "mul") == 0) return INST_MUL; 
     if(strcmp(str, "divi") == 0) return INST_DIVI; 
     if(strcmp(str, "jmp") == 0) return INST_JMP; 
-
+    if(strcmp(str, "dup") == 0) return INST_DUP;
+    if(strcmp(str, "cmp") == 0) return INST_CMP;
+    if(strcmp(str, "jz") == 0) return INST_JZ;
+    if(strcmp(str, "je") == 0) return INST_JE;
+    if(strcmp(str, "jnz") == 0) return INST_JNZ;
+    if(strcmp(str, "jne") == 0) return INST_JNE;
+    if(strcmp(str, "jg") == 0) return INST_JG;
+    if(strcmp(str, "jl") == 0) return INST_JL;
+    if(strcmp(str, "jge") == 0) return INST_JGE;
+    if(strcmp(str, "jnl") == 0) return INST_JNL;
+    if(strcmp(str, "jng") == 0) return INST_JNG;
+    if(strcmp(str, "jle") == 0) return INST_JLE;
+    
     fprintf(stderr, "unknown instruction\n");
     exit(EXIT_FAILURE);
 }
@@ -196,6 +240,9 @@ Trap vm_execute_inst(VM *vm, Inst inst){
 
     switch(inst.type){
 
+        case INST_NOP:
+            break;
+
         case INST_PUSH:      
             if(vm->sp >= VM_STACK_CAPACITY ) return TRAP_STACK_OVERFLOW;
 
@@ -243,9 +290,100 @@ Trap vm_execute_inst(VM *vm, Inst inst){
 
             vm->ip = inst.operand;
             return TRAP_OK;
-        
+
+        case INST_DUP:
+            if(inst.operand > vm->sp || inst.operand < 0) return TRAP_INDEX_OUT_OF_BOUND;
+
+            vm->stack[vm->sp] = vm->stack[inst.operand];
+            vm->sp++;
+            break;
+
+        case INST_CMP:
+            if(vm->sp < 2) return TRAP_STACK_UNDERFLOW;
+
+            if(vm->stack[vm->sp-2] == vm->stack[vm->sp-1]){
+                vm->stack[vm->sp-2] = CO_EQUAL;
+                vm->sp -= 1;
+            }else if(vm->stack[vm->sp-2] > vm->stack[vm->sp-1]){
+                vm->stack[vm->sp-2] = CO_GREATER;
+                vm->sp -= 1;
+            }else{
+                vm->stack[vm->sp-2] = CO_LESS;
+                vm->sp -= 1;
+            }    
+            break;
+
+        case INST_JE:
+        case INST_JZ:
+            if(vm->stack[vm->sp-1] > 2) return TRAP_UNEXPECTED_COMPARISON_OUTCOME;
+            if(vm->sp < 1) return TRAP_STACK_UNDERFLOW;
+            if(inst.operand < 0 || inst.operand > vm->program_size) return TRAP_INVALID_JMP;
+
+            if(vm->stack[vm->sp-1] == CO_EQUAL){ 
+                vm->ip = inst.operand;
+                return TRAP_OK;
+            }
+            break;
+
+        case INST_JNE:
+        case INST_JNZ:
+            if(vm->stack[vm->sp-1] > 2) return TRAP_UNEXPECTED_COMPARISON_OUTCOME;
+            if(vm->sp < 1) return TRAP_STACK_UNDERFLOW;
+            if(inst.operand < 0 || inst.operand > vm->program_size) return TRAP_INVALID_JMP;
+
+            if(vm->stack[vm->sp-1] != CO_EQUAL){ 
+                vm->ip = inst.operand;
+                return TRAP_OK;
+            }
+            break; 
+
+        case INST_JG:
+            if(vm->stack[vm->sp-1] > 2) return TRAP_UNEXPECTED_COMPARISON_OUTCOME;
+            if(vm->sp < 1) return TRAP_STACK_UNDERFLOW;
+            if(inst.operand < 0 || inst.operand > vm->program_size) return TRAP_INVALID_JMP;
+
+            if(vm->stack[vm->sp-1] == CO_GREATER){ 
+                vm->ip = inst.operand;
+                return TRAP_OK;
+            }
+            break; 
     
+        case INST_JL:
+            if(vm->stack[vm->sp-1] > 2) return TRAP_UNEXPECTED_COMPARISON_OUTCOME;
+            if(vm->sp < 1) return TRAP_STACK_UNDERFLOW;
+            if(inst.operand < 0 || inst.operand > vm->program_size) return TRAP_INVALID_JMP;
+
+            if(vm->stack[vm->sp-1] == CO_LESS){ 
+                vm->ip = inst.operand;
+                return TRAP_OK;
+            }
+            break;
+
+        case INST_JNL:
+        case INST_JGE:
+            if(vm->stack[vm->sp-1] > 2) return TRAP_UNEXPECTED_COMPARISON_OUTCOME;
+            if(vm->sp < 1) return TRAP_STACK_UNDERFLOW;
+            if(inst.operand < 0 || inst.operand > vm->program_size) return TRAP_INVALID_JMP;
+
+            if(vm->stack[vm->sp-1] != CO_LESS){ 
+                vm->ip = inst.operand;
+                return TRAP_OK;
+            }
+            break;
+
+        case INST_JLE:
+        case INST_JNG:
+            if(vm->stack[vm->sp-1] > 2) return TRAP_UNEXPECTED_COMPARISON_OUTCOME;
+            if(vm->sp < 1) return TRAP_STACK_UNDERFLOW;
+            if(inst.operand < 0 || inst.operand > vm->program_size) return TRAP_INVALID_JMP;
+
+            if(vm->stack[vm->sp-1] != CO_GREATER){ 
+                vm->ip = inst.operand;
+                return TRAP_OK;
+            }
+            break;
     }
+
     vm->ip++;
     return TRAP_OK;
 }
@@ -257,8 +395,7 @@ void dump_mem(VM vm){
 }
 
 
-int main2(void){
-
+int main(void){
     load_asm_from_file("./prog.vasm", &vm);
     while(vm.ip < vm.program_size && vm.ip >= 0){
         Trap trap = vm_execute_inst(&vm ,vm.program[vm.ip]);
@@ -267,15 +404,6 @@ int main2(void){
             return EXIT_FAILURE;
         }
     }
-    
     dump_mem(vm);
     return EXIT_SUCCESS;
-}
-
-
-void main(){
-    load_asm_from_file("./prog.vasm", &vm);
-    main2();
-
-
 }
